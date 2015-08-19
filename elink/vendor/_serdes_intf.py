ccd31cc9ce829508dd4f37b5af9a35fd4a47f0a6
Christopher Felton <chris.felton@gmail.com>
2015-08-19 06:26:38 -0500

    removed diff sig from interface

--- elink/vendor/_serdes_intf.py
+++ elink/vendor/_serdes_intf.py
@@ -17,25 +17,23 @@ class SERDESInterface(Vendor):
         self.clock_parallel = clock_parallel
 
         # serial bits input/output
-        self.serial_p = Signal(bool(0))
-        self.serial_n = Signal(bool(0))
+        self.serial = Signal(bool(0))
 
         # parallel data
         self.data = Signal(intbv(0)[nbits:])
 
-    def input_buffer(self, serial_in):
+    def input_buffer(self, seri_p, seri_n):
         @always_comb
         def rtl():
-            serial_in.next = self.serial_p and not self.serial_n
+            self.serial.next = seri_p and not seri_n
         return rtl,
 
-    def output_buffer(self, serial_out):
+    def output_buffer(self, sero_p, sero_n):
         @always_comb
         def rtl():
-            self.serial_p.next = serial_out
-            self.serial_n.next = not serial_out
+            sero_p.next = self.serial
+            sero_n.next = not self.serial
         return rtl,
 
     def get_signals(self):
         return (self.clock_serial, self.clock_parallel, self.data,)
-
